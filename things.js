b998cb9e5dcd0e2337b3db279eab1d548412419e
aikbix <ian@aikbix.com>
2013-09-20 19:58:41 -0700

    Updated documentation

--- things.js
+++ things.js
@@ -3,8 +3,15 @@
 // Web worker actor.
 // Single-threaded actor.
 
+
 var actorBase;
 
+
+/**
+ * Singleton. TODO
+ * @class WebWorkerActorDetail
+ * @classdesc A set of methods accessed by the actor that utilize web workers.
+ */
 function WebWorkerActorDetail() {
     'use strict';
 
@@ -20,6 +27,12 @@ function WebWorkerActorDetail() {
     self.broadcast = function (scope, message) {};
 }
 
+
+/**
+ * Singleton
+ * @class NativeActorDetail
+ * @classdesc A set of methods accessed by the actor to handle async behaviour.
+ */
 function NativeActorDetail() {
     'use strict';
 
@@ -30,14 +43,30 @@ function NativeActorDetail() {
     }
     NativeActorDetail.prototype.singletonInstance = self;
 
+    /**
+     * Posts message with setTimeout, calling the actor's on_message function.
+     * @param {Actor} scope - The Actor calling this method.
+     * @param {object} message - The message passed into the actor.
+     */
     self.put = function (scope, message) {
         setTimeout(function () { scope.on_message(message); }, 0);
     };
+
+    /**
+     * Synchronous calling of the actor's on_message function.
+     * @param {Actor} scope - The Actor calling this method.
+     * @param {object} message - The message passed into the actor.
+     */
 
     self.call = function (scope, message) {
         return scope.on_message(message);
     };
 
+    /**
+     * Broadcasts message to every actor subscribed to the actor passed in.
+     * @param {Actor} scope - The Actor calling this method.
+     * @param {object} message - The message passed to the actor.
+     */
     self.broadcast = function (scope, message) {
         var subscribers = scope.subscribers,
             actor,
@@ -51,7 +80,12 @@ function NativeActorDetail() {
 
 }
 
-// Singleton used to determine the actor implementation.
+
+/**
+ * Singleton
+ * @class ActorBase
+ * @classdesc Singleton used to determine the actor implementation.
+ */
 function ActorBase() {
     'use strict';
 
@@ -69,24 +103,46 @@ function ActorBase() {
         self.actorDetail = new NativeActorDetail();
     }
 
+    /**
+     * Passes put request to actor detail.
+     * @param {Actor} scope - The Actor calling this method.
+     * @param {object} message - The message passed to the actor.
+     */
     self.put = function (scope, message) {
         self.actorDetails.put(scope, message);
     };
 
+    /**
+     * Passes call request to actor detail.
+     * @param {Actor} scope - The Actor calling this method.
+     * @param {object} message - The message passed to the actor.
+     */
     self.call = function (scope, message) {
         self.actorDetail.call(scope, message);
     };
 
+    /**
+     * Passes broadcast request to actor detail.
+     * @param {Actor} scope - The Actor calling this method.
+     * @param {object} message - The message being broadcast.
+     */
     self.broadcast = function (scope, message) {
         self.actorDetail.broadcast(scope, message);
     };
 }
 
+
+/**
+ * @class
+ * @classdesc An actor.
+ * @param {Function} fn - A function that will handle recieved messages.
+ */
 function Actor(fn) {
     'use strict';
 
-    // @constructor
     var self = this;
+
+    // @constructor
     (function init() {
         if (typeof (fn) === 'function') {
             self.on_message = fn;
